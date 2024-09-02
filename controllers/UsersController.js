b72c545d2e982b8191e6666815743c230b10d202
Zkrallah <muhammad.heshamyt@gmail.com>
2024-09-02 23:55:40 +0300

    fix: I missed some modifications in the last commit.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import router from './routes/index';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use('/', router);
 
 app.listen(PORT, () => {
