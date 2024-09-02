91d6555f1f9f691c79899c4f54c89af7b3b60697
Zkrallah <muhammad.heshamyt@gmail.com>
2024-09-03 00:56:05 +0300

    feat: Custom auth logic implemented with cached tokens.

---
+++ controllers/AuthController.js
@@ -0,0 +1,49 @@
+import { v4 as uuidv4 } from 'uuid';
+import sha1 from 'sha1';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AuthController {
+  static async getConnect(req, res) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const base64credentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(base64credentials, 'base64').toString('utf-8');
+    const [email, passowrd] = credentials.split(':');
+
+    const hashedPassword = sha1(passowrd);
+    const user = await dbClient.db.collection('users').findOne({ email, password: hashedPassword });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const token = uuidv4();
+    const tokenKey = `auth_${token}`;
+    await redisClient.set(tokenKey, user._id.toString(), 86400); // store for 24 hrs
+
+    return res.status(200).json({ token });
+  }
+
+  static async getDisconnect(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const tokenKey = `auth_${token}`;
+    const userId = await redisClient.get(tokenKey);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    await redisClient.del(tokenKey);
+    return res.status(204).send();
+  }
+}
+
+export default AuthController;
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,8 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+const { ObjectId } = require('mongodb');
 
 class UsersController {
   static async postNew(req, res) {
@@ -40,6 +43,22 @@ class UsersController {
       id: result.insertedId,
       email,
     });
+  }
+
+  static async getMe(req, res) {
+    const token = req.header('X-Token') || null;
+    if (!token) return res.status(401).send({ error: 'Unauthorized' });
+
+    const redisToken = await redisClient.get(`auth_${token}`);
+    if (!redisToken) return res.status(401).send({ error: 'Unauthorized' });
+
+    const user = await dbClient.db
+      .collection('users')
+      .findOne({ _id: ObjectId(redisToken) });
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+    delete user.password;
+
+    return res.status(200).send({ id: user._id, email: user.email });
   }
 }
 
