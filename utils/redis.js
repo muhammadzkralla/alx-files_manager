13378432049aea288aa9134bc181f3e24b88c115
Zkrallah <muhammad.heshamyt@gmail.com>
2024-09-02 22:47:43 +0300

    feat: Redis client implemented for caching.

---
+++ utils/redis.js
@@ -0,0 +1,53 @@
+const redis = require('redis');
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+        
+        this.client.on('error', (err) => {
+            console.error('Redis error:', err);
+        });
+    }
+
+    isAlive() {
+        // Check if the client is connected
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, reply) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(reply);
+            });
+        });
+    }
+
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.setex(key, duration, value, (err) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(reply);
+            });
+        });
+    }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
+
